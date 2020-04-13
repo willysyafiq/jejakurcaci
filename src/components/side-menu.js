5382d14ce345d7dabe224cc8fa2eb54ea143c129
willysyafiq <willy.syafiq@gmail.com>
2020-04-13 09:45:24 +0700

    updated mega menu

--- src/components/side-menu.js
+++ src/components/side-menu.js
@@ -1,12 +1,99 @@
 import React from 'react';
 import './style.scss';
 import hamburger from '../images/hamburger-white.svg';
+import close from '../images/close-white.svg';
+import { slide as Menu } from 'react-burger-menu';
+import featured1 from '../images/image-1.png';
+import chevronRight from '../images/chevron-right.svg';
+import { Link } from 'gatsby';
 
 const SideMenu = () => (
-    <section className="side-menu">
-        <div className="section">
+    <section>
+        {/* <a className="hamburger-sidebar"> 
             <img className="image menu-icon" src={hamburger} alt="hamburger" />
-        </div>
+        </a> */}
+        <Menu width={'100%'} customBurgerIcon={ <img src={hamburger} /> } customCrossIcon={ <img src={close} /> }>
+            <div className="columns">
+                <div className="column is-8 first-half">
+                    <ul>
+                        <li>
+                            <a href="#">LOVE STORIES</a>
+                            <div className="menu-sub">
+                                <div className="columns">
+                                    <div className="column is-7">
+                                        <ul>
+                                            <li><a href="#">Couples and Lovebirds <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Ceremonials <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Tying the Knot <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Folks and Families <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="column is-5">
+                                    <Link to="/" className="block-highlight">
+                                        <img className="image" src={featured1} alt="thumbnail" />
+                                        <p className="highlight">HIGHLIGHT</p>
+                                        <p to="" className="title">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit</p>
+                                        <Link to="/" className="read-more">READ MORE</Link>
+                                    </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">PROJECTS</a>
+                            <div className="menu-sub">
+                                <div className="columns">
+                                    <div className="column is-7">
+                                        <ul>
+                                            <li><a href="#">Editorials <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Portraits <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Wanderlust <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Miscellanous <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="column is-5">
+                                    <Link to="/" className="block-highlight">
+                                        <img className="image" src={featured1} alt="thumbnail" />
+                                        <p className="highlight">HIGHLIGHT</p>
+                                        <p to="" className="title">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit</p>
+                                        <Link to="/" className="read-more">READ MORE</Link>
+                                    </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">JOURNAL</a>
+                            <div className="menu-sub">
+                                <div className="columns">
+                                    <div className="column is-7">
+                                        <ul>
+                                            <li><a href="#">Pople <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Places <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                            <li><a href="#">Other Stories <img className="image chevron-menu" src={chevronRight} alt="chevron right" /></a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="column is-5">
+                                    <Link to="/" className="block-highlight">
+                                        <img className="image" src={featured1} alt="thumbnail" />
+                                        <p className="highlight">HIGHLIGHT</p>
+                                        <p to="" className="title">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit</p>
+                                        <Link to="/" className="read-more">READ MORE</Link>
+                                    </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a className="no-bot-border"  href="#">ABOUT</a>
+                        </li>
+                    </ul>       
+                </div>
+                <div className="column is-3 second-half">
+                    {/* <img className="image" src={featured1} alt="thumbnail" /> */}
+                </div>
+            </div>
+        </Menu>
     </section>
 );
 
---
+++ src/pages/testing.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../components/style.scss';
+import LayoutHome from '../components/layout-home';
+import hamburger from '../images/hamburger-white.svg';
+
+class Example extends React.Component {
+//   showSettings (event) {
+//     event.preventDefault();
+    
+//   }
+
+  render () {
+    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    return (
+        
+        <LayoutHome>
+
+        </LayoutHome>
+
+    );
+  }
+}
+
+export default Example;
